f0f51b09831db3138d15702bdabd6a961a2f4f1e
Michael Naumov <vommuan@gmail.com>
2026-03-05 12:50:50 +0700

    ClientInterface and factories interfaces were divided

--- tests/Unit/adapters/yii/ClientTest.php
+++ tests/Unit/adapters/yii/ClientTest.php
@@ -3,8 +3,6 @@
 
 namespace Tests\Unit\adapters\yii;
 
-use nsusoft\captcha\adapters\yii\Client;
-use Psr\Http\Message\RequestInterface;
 use Tests\Support\UnitTester;
 
 class ClientTest extends \Codeception\Test\Unit
@@ -17,51 +15,6 @@ class ClientTest extends \Codeception\Test\Unit
     }
 
     public function testSendRequest()
-    {
-        $this->markTestIncomplete();
-    }
-
-    public function testСreateRequest()
-    {
-        $client = new Client();
-        $request = $client->createRequest('GET', '/test');
-
-        $this->assertInstanceOf(RequestInterface::class, $request);
-        $this->assertEquals('GET', $request->getMethod());
-        $this->assertEquals('/test', $request->getUri());
-    }
-
-    public function testCreateResponse()
-    {
-        $this->markTestIncomplete();
-    }
-
-    public function testCreateServerRequest()
-    {
-        $this->markTestIncomplete();
-    }
-
-    public function testCreateStream()
-    {
-        $this->markTestIncomplete();
-    }
-
-    public function testCreateStreamFromFile()
-    {
-        $this->markTestIncomplete();
-    }
-
-    public function testCreateStreamFromResource()
-    {
-        $this->markTestIncomplete();
-    }
-
-    public function testCreateUploadedFile()
-    {
-        $this->markTestIncomplete();
-    }
-
-    public function testCreateUri()
     {
         $this->markTestIncomplete();
     }
