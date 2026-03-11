8a856ec80d51fa033e588aa62a7604fcf0b94213
Michael Naumov <vommuan@gmail.com>
2026-03-11 12:28:48 +0700

    Test component configuration

--- config/test.php
+++ config/test.php
@@ -11,27 +11,9 @@ $captcha = require __DIR__ . '/captcha.php';
 return [
     'id' => 'library-tests',
     'basePath' => dirname(__DIR__),
-    // 'aliases' => [
-    //     '@bower' => '@vendor/bower-asset',
-    //     '@npm'   => '@vendor/npm-asset',
-    // ],
-    // 'language' => 'en-US',
     'components' => [
-        // 'assetManager' => [
-        //     'basePath' => dirname(__DIR__) . '/web/assets',
-        // ],
         'captcha' => ArrayHelper::merge($captcha, [
             'class' => Cap::class,
-            'client' => [
-                'class' => \yii\httpclient\Client::class,
-            ],
         ]),
-        // 'request' => [
-        //     'cookieValidationKey' => 'test',
-        //     'enableCsrfValidation' => false,
-        // ],
-        // 'urlManager' => [
-        //     'showScriptName' => true,
-        // ],
     ],
 ];
